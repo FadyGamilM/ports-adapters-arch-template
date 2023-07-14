51f3383e7849a3b24af9de39a6a3a5cc2665af45
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-07-14 17:40:59 +0300

    FEATURE | inject the database from framework-layer into application-layer

--- internals/adapters/application/api/api.go
+++ internals/adapters/application/api/api.go
@@ -9,15 +9,27 @@ import (
 type Adapter struct {
 	// the application layer implementation depends on the domain (core) layer interface (port)
 	financial_acc ports.FinancialAccountPort
+
+	// the application layer implementation used the framework layer (DI)
+	db ports.DbPort
 }
 
 func (adapter *Adapter) PerformDeposite(amount float32) (float32, error) {
 	log.Println("APPLICATION LAYER | PerformDeposite() method is called")
+	// use core layer business logic
 	curr_balance, err := adapter.financial_acc.Deposite(amount)
 	if err != nil {
 		log.Println("APPLICATION LAYER | Error occurred at the core layer while executing the Deposite()")
 		return 0, err
 	}
+
+	// save to database
+	err = adapter.db.SaveToHistory(curr_balance, "DEPOSITE")
+	if err != nil {
+		log.Printf("APPLICATION LAYER | Error while calling SaveToHIstory() on database : %v \n", err)
+		return 0, err
+	}
+
 	return curr_balance, nil
 }
 
@@ -31,6 +43,6 @@ func (adapter *Adapter) PerformWithdraw(amount float32) (float32, error) {
 	return curr_balance, nil
 }
 
-func NewAdapter(fa ports.FinancialAccountPort) *Adapter {
-	return &Adapter{financial_acc: fa}
+func NewAdapter(fa ports.FinancialAccountPort, db ports.DbPort) *Adapter {
+	return &Adapter{financial_acc: fa, db: db}
 }
